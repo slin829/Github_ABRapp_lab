e1534bcaaf3a051fa0a01d6514a9a2311e091134
LuxB2000 <j.plumat@gmail.com>
2015-03-20 13:37:06 +1300

    test

--- createSummary.R
+++ createSummary.R
@@ -10,6 +10,26 @@ createSummary <- function( refData, newData = "NULL" ){
   # The output
   summary <- list(result="OK", freqProb=c("cliks","4K","8k"), metric=.95); 
   
+  #
+  # init
+  #
+  availf = c("clicks", "4k","8k", "12k","16k","20k","24k","28k");
+  
+  for( freq in availf ){
+    
+    #
+    # Load the reference data set
+    #
+    
+    #
+    # Load the data set to compare with
+    #
+    
+    #
+    # compare each frequency of the newData to the reference prediction interval
+    #
+    
+  }
   
   return( summary );
   
