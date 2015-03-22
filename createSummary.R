31823ef5713e92f837485358a2b2b287393345ce
LuxB2000 <j.plumat@gmail.com>
2015-03-23 10:45:17 +1300

    Create a standard way to load data.

--- createSummary.R
+++ createSummary.R
@@ -4,37 +4,37 @@
 # create a summary based on reference data (refData) and new data (newData). The function will return a strcutur
 # that contains the result of the summary (OK, KO). If the result is KO thant additional information about the
 # frequencies with problems as a list.
+# Example: createSummary( refData="ABR_RZ6_C57", newData="")
 #
 
 createSummary <- function( refData, newData = "NULL" ){
-  # The output
-  summary <- list(result="OK", freqProb=c("cliks","4K","8k"), metric=.95); 
+  Dir= getwd();
+  source(paste0(Dir,'/funs','/load_data.R'));
+  
+  
+  # Initiate the output
+  summary <- list(result="noData", freqProb=c("cliks","4K","8k"), metric=.95); 
   
   #
   # init
   #
-  availf = c("clicks", "4k","8k", "12k","16k","20k","24k","28k");
   refFolder = "./ABR_standards/";
+  newFolder = "ABR_ID"; # TODO make it parameters
+  
+  # Load the reference data set
+  refData <- load_data( paste0(refFolder,refData) );
+  
+  # Load the data set to compare with
+  if( is.null(newData) ){
+    # No data to compare with
+    summary <- list(result="noData", freqProb=c(), metric=Inf); 
+    return( summary );
+  }else{
+    newData <- load_data( paste0(newFolder,newData) );
+  }
+    
   
   for( freq in availf ){
-    
-    var = dir(paste0(refFolder, system));
-    var= as.vector(var);
-    # TODO: create a funciton to load data in a standard way.
-    
-    #
-    # Load the reference data set
-    #
-    
-    #check if the frequence exits in the reference folder
-    if( !(paste0(freq,".csv")) %in% dir(paste("./ABR_standards/", refData, "/")) ){
-      # TODO: add freq in freqProb?
-      next
-    }
-    
-    #
-    # Load the data set to compare with
-    #
     
     #
     # compare each frequency of the newData to the reference prediction interval
---
+++ funs/load_data.R
@@ -0,0 +1,10 @@
+#
+# load a data set that could be either the reference data either a new data to analyse, this function load a complete folder
+# return a data structure with freq name and data value
+
+data <- function(path_to_folder){
+  availf = c("clicks", "4k","8k", "12k","16k","20k","24k","28k");
+  
+  data <- list( freq_name=c(), val=c() );
+  
+}
