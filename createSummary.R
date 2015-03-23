6c71648c231fa248a696cebfa3c47fe5bf59a4b3
LuxB2000 <j.plumat@gmail.com>
2015-03-23 14:59:14 +1300

    load data as reference or new data

--- createSummary.R
+++ createSummary.R
@@ -4,41 +4,40 @@
 # create a summary based on reference data (refData) and new data (newData). The function will return a strcutur
 # that contains the result of the summary (OK, KO). If the result is KO thant additional information about the
 # frequencies with problems as a list.
-# Example: createSummary( refData="ABR_RZ6_C57", newData="")
+# Example: createSummary( refDataID="ABR_RZ6_C57 (heating)", newDataFolder="abr_data_shelly", newDataID="A1101")
 #
 
-createSummary <- function( refData, newData = "NULL" ){
+createSummary <- function( refDataID, newDataFolder="NULL", newDataID = "NULL" ){
   Dir= getwd();
   source(paste0(Dir,'/funs','/load_data.R'));
   
   
   # Initiate the output
-  summary <- list(result="noData", freqProb=c("cliks","4K","8k"), metric=.95); 
+  summary <- list(result="noDataSelected", freqProb=c("cliks","4K","8k"), metric=.95); 
   
   #
   # init
   #
-  refFolder = "./ABR_standards/";
+  refFolder = "ABR_standards";
   newFolder = "ABR_ID"; # TODO make it parameters
   
-  # Load the reference data set
-  refData <- load_data( paste0(refFolder,refData) );
   
   # Load the data set to compare with
-  if( is.null(newData) ){
+  if( is.null(newDataID) || is.null(newDataFolder) ){
     # No data to compare with
     summary <- list(result="noData", freqProb=c(), metric=Inf); 
     return( summary );
   }else{
-    newData <- load_data( paste0(newFolder,newData) );
+    newData <- load_data( paste0(getwd(),"/",newFolder,"/",newDataFolder,"/",newDataID) );
   }
-    
   
+  # Load the reference data set
+  refData <- load_data( paste0(getwd(),"/",refFolder,"/",refDataID) , TRUE);
+  
+  #
+  # Compare each frequency of the newData to the reference prediction interval
+  #
   for( freq in availf ){
-    
-    #
-    # compare each frequency of the newData to the reference prediction interval
-    #
     
   }
   
--- funs/load_data.R
+++ funs/load_data.R
@@ -1,10 +1,65 @@
 #
+# data <- function(path_to_folder)
 # load a data set that could be either the reference data either a new data to analyse, this function load a complete folder
 # return a data structure with freq name and data value
+# Example: load_data("~/Workspace/R/ABR/ABR_standards/ABR_RZ6_C57 (heating)")
 
-data <- function(path_to_folder){
-  availf = c("clicks", "4k","8k", "12k","16k","20k","24k","28k");
+load_data <- function(path_to_folder, recursive=FALSE){
+  availf <- c("clicks", "4k","8k", "12k","16k","20k","24k","28k");
+  nbrOfF <- length(availf);
+  ext <- ".csv";
+  mesF = c();
   
-  data <- list( freq_name=c(), val=c() );
+  # initiate output
+  data <- list( freq_name=vector(), N=vector(), val=list() );
   
+  # get all the folder containing all the possible frequencies in the folder
+  if( recursive ) {
+    animal_folders <- as.vector( dir(path_to_folder) );
+  }else{
+    animal_folders <- c(".");
+  }
+  
+  # parse each animal folder, find frequency files
+  i = 0; last_i = 0;
+  for( f in availf ){
+    
+    Data = vector();
+    N = 0;
+    for( fold in animal_folders ){
+      
+      if( (paste0(f,ext)) %in% dir(paste0(path_to_folder,"/",fold)) ){
+          print( paste0("Ready to do ", fold, " and frequence ", f) );
+          # there is a file for this frequency
+          d = read.csv(paste0(path_to_folder,"/",fold,"/",f,ext), header=TRUE);
+          d = cbind(d$Sub..ID, d$Level.dB., d$T1.ms., d$V1.uv., d$T2.ms., d$V2.uv.);
+          d = as.data.frame(d);
+          colnames(d) <- c("Sub_ID", "Sound_level", "Peak_lat", "Peak_amp", "Trough_lat", "Trough_amp")
+          N = N + length(d$Sub_ID);
+          Data <- rbind(Data, d);
+          
+          # check if we already measure this frequency
+          if( sum(mesF == f) == 0 ){
+            mesF[length(mesF)+1] = f;
+          }
+          
+          if( !recursive ) { i = i + 1; }
+      }
+     
+    }# end for folder
+    
+    # output
+    if( last_i != i){
+      data$freq_name=mesF;
+      data$N[i] = N;
+      data$val[[i]]=Data;
+      last_i = i;
+    }
+    
+    if( recursive ){ i = i +1; }
+    
+  } # end for frequency
+  
+  
+  return(data);
 }
